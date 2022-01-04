cfb49e72b1c50ec09abf4798e4db7ea9c79cac7a
Pmupenyu <96637541+Pmupenyu@users.noreply.github.com>
2022-01-04 17:51:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
+import re
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from pymongo import MongoClient
 from datetime import datetime
 
+# Database
+
 cluster = MongoClient("mongodb+srv://polingony:UmnASDTkKZfPJvFA@extralearningcluster.drdtq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = cluster["e-learning"]
 users = db["users"]
-level = db["level"]
 courses = db["courses"]
+contacts = db["contacts"]
+subscription = db["subscription"]
 subscriptions = db["subscriptions"]
+demo_users = db["demo_users"]
+freemium_users = db["freemium_users"]
+premium_users = db["premium_users"]
+super_users = db["super_users"]
+# Primary Database
+grade_one = db["grade_one"]
+grade_two = db["grade_two"]
+grade_three = db["grade_three"]
+grade_four = db["grade_four"]
+grade_five = db["grade_five"]
+grade_six = db["grade_six"]
+grade_seven = db["grade_seven"]
+# Secondary Database
+form_one = db["form_one"]
+form_two = db["form_two"]
+form_three = db["form_three"]
+form_four = db["form_four"]
+form_five = db["form_five"]
+form_six = db["form_six"]
+# Duration Database
+day_one = db["day_one"]
+
+register_mode = db["register_mode"]
+details_mode = db["register_mode"]
+help_mode = db["help_mode"]
 
 app = Flask(__name__)
 
@@ -20,38 +49,64 @@ def reply():
     number = number.replace("whatsapp:", "")[:-2]
     res = MessagingResponse()
     user = users.find_one({"number": number})
+
+    # Checking user is in db
+
     if bool(user) == False:
-        msg = res.message("Hi, welcome to *Extra Learning* the Best e-learning Platform for all.\nYou can choose from one of the options below: "
-                    "\n\n*Type*\n\n 1️⃣ To *contact* us \n 2️⃣ To *order* snacks \n 3️⃣ To know our *working hours* \n 4️⃣ "
-                    "To get our *address*")
+        msg = res.message("Hello and Welcome to *Extra Learning* one of the best *e-learning* platform in *Zimbabwe*. "
+                "\n\nYou will be learning wherever you are and whenever you want using your Smartphone,Tablet or Personal Computer"
+                "using your WhatsApp"
+                "\n\nTo get Started Respond with the option of your choice using numbers:"
+                    "\n\n*Type*\n\n 1️⃣ Register \n 2️⃣ Details \n 3️⃣ Demo \n 4️⃣ "
+                    "Help  \n")
         msg.media("https://i.ibb.co/BPKnXVP/Red-Velvet-Cake-Waldorf-Astoria.jpg")
         users.insert_one({"number": number, "status": "main", "messages": []})
+
+    # Main Status and options
+
     elif user["status"] == "main":
         try:
             option = int(text)
         except:
-            res.message("Please enter a valid response")
+            res.message("Please enter a *valid* response or use *numbers* to respond in this *mode*")
             return str(res)
 
         if option == 1:
-            res.message(
-                "You can contact us through phone or e-mail.\n\n*Phone*: 991234 56789 \n*E-mail* : contact@theredvelvet.io")
+            res.message("📝*YOU ARE NOW IN REGISTRATION MODE*")
+            res.message("1️⃣ Primary Education \n\n2️⃣ secondary Education \n\n3️⃣ Courses\n\n4️⃣ About Us \n\n5️⃣ Help \n\n0️⃣ Main Menu")
+            register_mode.update_one(
+                {"number": number}, {"$set": {"status": "details"}})
         elif option == 2:
-            res.message("You have entered *ordering mode*.")
-            users.update_one(
-                {"number": number}, {"$set": {"status": "ordering"}})
+            res.message("📜*DETAILS MODE*.")
+            details_mode.update_one(
+                {"number": number}, {"$set": {"status": "details"}})
             res.message(
-                "You can select one of the following cakes to order: \n\n1️⃣ Red Velvet  \n2️⃣ Dark Forest \n3️⃣ Ice Cream Cake"
-                "\n4️⃣ Plum Cake \n5️⃣ Sponge Cake \n6️⃣ Genoise Cake \n7️⃣ Angel Cake \n8️⃣ Carrot Cake \n9️⃣ Fruit Cake  \n0️⃣ Go Back")
+                "You can select one of the following cakes to order: \n\n1️⃣ Primary Education  \n2️⃣ Secondary Education \n3️⃣ Courses"
+                "\n4️⃣ About Us \n5️⃣ Help  \n0️⃣ Go Back")
         elif option == 3:
-            res.message("We work from *9 a.m. to 5 p.m*.")
+            res.message("📜*DEMO MODE*.")
+            demo_users.update_one(
+                {"number": number}, {"$set": {"status": "demo"}})
+            res.message("1️⃣ Primary Education \n\n2️⃣ secondary Education \n\n3️⃣ Courses\n\n4️⃣ Help \n\n0️⃣ Main Menu")
 
         elif option == 4:
-            res.message(
-                "We have multiple stores across the city. Our main center is at *4/54, New Delhi*")
+            res.message("📜*HELP MODE*.")
+            res.message("1️⃣ English \n\n2️⃣ Shona \n\n3️⃣ Contacts \n\n4️⃣ About Us \n\n0️⃣ Main Menu")
+            help_mode.update_one(
+                {"number": number}, {"$set": {"status": "details"}})
         else:
-            res.message("Please enter a valid response")
-    elif user["status"] == "ordering":
+            res.message("Please enter a *valid* response or use *numbers* to respond in this *mode*")
+
+
+    # Registering Status and options
+    
+    if bool(freemium_users) == False:
+        msg = res.message("Placeholder")
+        msg = res.message("Placeholder")
+        msg.media("https://i.ibb.co/BPKnXVP/Red-Velvet-Cake-Waldorf-Astoria.jpg")
+        users.insert_one({"number": number, "status": "main", "messages": []})
+
+    elif user["status"] == "registration":
         try:
             option = int(text)
         except:
@@ -75,13 +130,32 @@ def reply():
             res.message("Please enter your address to confirm the order")
         else:
             res.message("Please enter a valid response")
+
+            # Registering Status and options
+
     elif user["status"] == "address":
         selected = user["item"]
         res.message("Thanks for shopping with us 😊")
         res.message(f"Your order for *{selected}* has been received and will be delivered within an hour")
-        subscriptions.insert_one({"number": number, "item": selected, "address": text, "order_time": datetime.now()})
+        orders.insert_one({"number": number, "item": selected, "address": text, "order_time": datetime.now()})
         users.update_one(
             {"number": number}, {"$set": {"status": "ordered"}})
+
+
+            # Registering Status and options
+
+    elif user["status"] == "address":
+        selected = user["item"]
+        res.message("Thanks for shopping with us 😊")
+        res.message(f"Your order for *{selected}* has been received and will be delivered within an hour")
+        orders.insert_one({"number": number, "item": selected, "address": text, "order_time": datetime.now()})
+        users.update_one(
+            {"number": number}, {"$set": {"status": "ordered"}})
+
+
+
+            
+            #Checking first time user or Registering Status and options
     elif user["status"] == "ordered":
         res.message("Hi, thanks for contacting again.\nYou can choose from one of the options below: "
                     "\n\n*Type*\n\n 1️⃣ To *contact* us \n 2️⃣ To *order* snacks \n 3️⃣ To know our *working hours* \n 4️⃣ "
