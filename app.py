6de600c8947558fd90ba9151036320037dd40b95
Pmupenyu <pmupenyu@gmail.com>
2022-01-30 19:16:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1063,10 +1063,13 @@ def reply():
             res.message(f"Hello {fname} You're Premium user 😊")
             users.update_one(
             {"number": number}, {"$set": {"status": "main"}})
-        elif bool (usersub) == False:
+        else:
             res.message("It looks like you're not registered")
+            res.message("   📝 *YOU ARE NOW IN REGISTRATION MODE :*")
+            res.message("1️⃣ Primary Education \n\n2️⃣ secondary Education \n\n3️⃣ Courses\n\n4️⃣ About Us \n\n5️⃣ Help \n\n0️⃣ Main Menu")
             users.update_one(
-            {"number": number}, {"$set": {"status": "main"}})
+                {"number": number}, {"$set": {"status": "registration"}})
+
 
 
             # Login Status
