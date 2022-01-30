1e54cc349d4117207d2c4c7a1cb51b3543d3740f
Pmupenyu <pmupenyu@gmail.com>
2022-01-31 01:06:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1055,6 +1055,7 @@ def reply():
             res.message("Please enter a valid response")
             return str(res)
         fname = user["firstname"]
+        subs = user["subscription"]
         if user["subscription"] == "freemium":
             res.message(f"👋🏼 Hello *{fname}* you're a *_freemium_* user you will have limited content")
             res.message("💻 Enter your _password_ to continue...")
@@ -1064,7 +1065,7 @@ def reply():
             res.message(f"👋🏼 Hello *{fname}* You're a Premium user 🥇")
             users.update_one(
             {"number": number}, {"$set": {"status": "main"}})
-        elif bool (user["subscription"]) == False:
+        elif bool (subs) == False:
             res.message("It looks like you're not registered")
             users.update_one(
             {"number": number}, {"$set": {"status": "main"}})
