81e0838dc3e6c579f297d046ac10ca79ef36f51c
Pmupenyu <pmupenyu@gmail.com>
2022-01-31 02:26:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1042,9 +1042,13 @@ def reply():
             #Content Placeholder
 
     elif user["status"] == "content":
-        res.message("*Extra Learning* is one of the best *e-learning* platform in *Zimbabwe*. "
-                "\n\nYou will be learning wherever you are and whenever you want using your Smartphone,Tablet or Personal Computer"
-                "using your WhatsApp. \n\n Waiting for your Tutor to wake up or come online is now thing of the past."
+        pw = user["password"]
+        try:
+            pssword = (text)
+        except:
+            res.message("Please enter valid password")
+        if user["password"] == pssword:
+            res.message("Congrats your Password works and accepted"
                 "\n\nTo get Started Respond with the option of your choice using numbers:"
                     "\n\n*Type*\n\n 1️⃣ Register \n 2️⃣ Login \n 3️⃣ Demo \n 4️⃣ "
                     "Help  \n")
@@ -1068,8 +1072,9 @@ def reply():
         elif user["subscription"] == "premium":
             fname = user["firstname"]
             res.message(f"👋🏼 Hello *{fname}* You're a Premium user 🥇")
+            res.message("💻 Enter your _password_ to continue...")
             users.update_one(
-            {"number": number}, {"$set": {"status": "main"}})
+            {"number": number}, {"$set": {"status": "content"}})
         elif user["subscription"] == "none":
             res.message("It looks like you're not registered\n\n You are now being taken to Registration mode")
             res.message("   📝 *YOU ARE NOW IN REGISTRATION MODE :*")
